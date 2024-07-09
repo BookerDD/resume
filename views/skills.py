f29ffc2411fe772c0d6ad5e85b6cd8df8539e8fe
Kalyvan <fatherandr@gmail.com>
2024-07-09 14:58:04 +0300

    Resume

--- views/skills.py
+++ views/skills.py
@@ -2,14 +2,25 @@ import streamlit as st
 
 st.title('Навыки')
 
-st.markdown("""
-    - Python
-    - PostgreSQL
-    - MySQL
-    - PyQt6
-    - Mathplotlib
-    - Git & GitHub
-    - Pandas
+col1, col2 = st.columns(2, gap='large', vertical_alignment='center')
+with col1:
+    st.title('Hard Skills')
+    st.markdown("""
+        - Python
+        - PostgreSQL
+        - MySQL
+        - PyQt6
+        - Mathplotlib
+        - Git & GitHub
+        - Pandas
+    """)
+with col2:
+    st.title('Soft Skills')
+    st.markdown("""
+        Soft skills
+        - Работа в команде
+        - Коммуникация
+        - Отзывчивость
 """)
 
 
